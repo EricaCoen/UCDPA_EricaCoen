5dc4d96bac09af410546aa18d64d6342e7f70e78
ecoen <erica.coen@gmail.com>
2021-05-17 15:38:51 +0100

    "Fourth Commit"

--- UCDPA_EricaCoen_FinalProject.py
+++ UCDPA_EricaCoen_FinalProject.py
@@ -3,6 +3,12 @@ import matplotlib
 import pandas as pd
 data = pd.read_csv("heart.csv")
 print(data.head())
+data.columns = ['Age','Sex','Chest Pain Type','Resting Blood Pressure','Serum Cholesterol in mg/dl','Fasting Blood Sugar > 120mg/dl','Resting ECG Results','Max HR Achieved','Exercise Induced Angina','Oldpeak','Slope','Number Major Vessels','Thal','Target']
+print(data.head())
+# Where Thal measurement; 0 = Normal, 1 = Fixed Defect and 2 = Reversible Defect
+
+
+
 
 
 
